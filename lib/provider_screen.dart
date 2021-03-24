0716be1f70223b955c09ba7bb162b379fde620b1
SinaSystem <sinasystem47@gmail.com>
2021-03-25 02:25:52 +0430

    Commit StateProvider

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/text_widget.dart';
 
-//We cannot change this Provider
-final numberProvider = Provider<int>((ref) => 43);
+//We can change the state
+final stateProvider = StateProvider<int>((ref) => 0);
+//final stateProvider = StateProvider.autoDispose<int>((ref) => 0);
 
 class ProviderScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
+    final provider = watch(stateProvider);
     return Scaffold(
-      body: Center(
-        child: TextWidget(
-          watch(numberProvider).toString()
-        )
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          //Line below not recommended
+         // provider.state++;
+          final provider = context.read(stateProvider);
+          provider.state++;
+        },
       ),
+      body: Center(
+          child: TextWidget(provider.state.toString())),
     );
   }
 }
