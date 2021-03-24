99d20497868c3f593f487dd23eba41c7a872b22d
SinaSystem <sinasystem47@gmail.com>
2021-03-25 02:47:39 +0430

    Commit StreamProvider

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -2,24 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/text_widget.dart';
 
-Future<int> fetchDate() async {
-  await Future.delayed(Duration(seconds: 2));
-  return 20;
+
+
+
+final streamProvider = StreamProvider<String>(
+      (ref) => Stream.periodic(Duration(seconds: 1), (count) => count.toString()),
+);
+
+Widget buildStreamBuilder(watch) {
+  final stream = watch(streamProvider.stream);
+  return StreamBuilder<String>(
+    stream: stream,
+    builder: (context, snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.waiting:
+          return CircularProgressIndicator();
+        default:
+          if (snapshot.hasError) {
+            return TextWidget('Error: ${snapshot.error}');
+          } else {
+            final counter = snapshot.data;
+            return TextWidget(counter);
+          }
+      }
+    },
+  );
 }
 
-final futureProvider = FutureProvider<int>((ref) => fetchDate());
-//final futureProvider = FutureProvider.autoDispose<int>((ref)=>fetchDate());
+Widget buildStreamWhen(watch) {
+  final stream = watch(streamProvider);
+
+  return stream.when(
+    data: (value) => TextWidget(value),
+    loading: () => CircularProgressIndicator(),
+    error: (e, stack) => TextWidget('Error: $e'),
+  );
+}
 
 class ProviderScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
-    final future = watch(futureProvider);
     return Scaffold(
       body: Center(
-        child: future.when(
-            data: (value) => TextWidget(value.toString()),
-            loading: () => CircularProgressIndicator(),
-            error: (error, stack) => TextWidget("Error : $error")),
+        child: buildStreamWhen(watch),
       ),
     );
   }
