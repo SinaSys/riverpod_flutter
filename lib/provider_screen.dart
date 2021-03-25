692f999b67c54496e9465d78464e5e92d475b533
SinaSystem <sinasystem47@gmail.com>
2021-03-25 13:06:23 +0430

    Commit StateNotifier

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -3,37 +3,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/text_widget.dart';
 
+import 'button_widget.dart';
 
-final carProvider = ChangeNotifierProvider<CarNotifier>((ref) => CarNotifier());
+final stateNotifierProvider =
+    StateNotifierProvider<CarNotifier>((ref) => CarNotifier());
 
 class ProviderScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
-    final car = watch(carProvider);
+    final car = watch(stateNotifierProvider.state);
+    final speed = car.speed;
+    final doors = car.doors;
+    final carNotifier = watch(stateNotifierProvider);
 
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextWidget('Speed: ${car._speed}'),
+            TextWidget('Speed: $speed'),
             const SizedBox(height: 8),
+            TextWidget('Doors: $doors'),
+            const SizedBox(height: 32),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  child:Text('Increase +5'),
-                  onPressed:()=> car.increase(),
-                ),
-                // car.increase: not efficient
-                // => button rebuilds every time if car state changes
+                ButtonWidget('Increase +5', onClicked: carNotifier.increaseSpeed),
                 const SizedBox(width: 12),
-                ElevatedButton(
-                  child: Text('decrease -30'),
-                  onPressed:()=> context.read(carProvider).decrease(),
-                ),
+                ButtonWidget('Decrease -30', onClicked: carNotifier.hitBrake),
               ],
             ),
+            const SizedBox(height: 32),
+            Slider(
+              value: car.doors.toDouble(),
+              max: 5,
+              onChanged: (value) => carNotifier.setDoors(value.toInt()),
+            )
           ],
         ),
       ),
@@ -41,17 +46,43 @@ class ProviderScreen extends ConsumerWidget {
   }
 }
 
-class CarNotifier extends ChangeNotifier {
-  int _speed = 120;
+class Car {
+  final int speed;
+  final int doors;
 
-  void increase() {
-    _speed += 5;
-    notifyListeners();
+  const Car({
+    this.speed = 120,
+    this.doors = 4,
+  });
+
+  Car copy({
+    int speed,
+    int doors,
+  }) =>
+      Car(
+        speed: speed ?? this.speed,
+        doors: doors ?? this.doors,
+      );
+}
+
+class CarNotifier extends StateNotifier<Car> {
+  CarNotifier() : super(Car());
+
+  void setDoors(int doors) {
+    final newState = state.copy(doors: doors);
+    state = newState;
   }
 
-  void decrease() {
-    _speed = max(0, _speed - 30);
-    notifyListeners();
+  void increaseSpeed() {
+    final speed = state.speed + 5;
+    final newState = state.copy(speed: speed);
+    state = newState;
+  }
+
+  void hitBrake() {
+    final speed = max(0, state.speed - 30);
+    final newState = state.copy(speed: speed);
+    state = newState;
   }
 
 // @override
