1829440947adf90198328f312c33b3b41c911131
SinaSystem <sinasystem47@gmail.com>
2021-04-15 13:23:49 +0430

    Commit Stream Provider Method Two

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/text_widget.dart';
 
-final streamProvider = StreamProvider<String>(
+final streamProvider = StreamProvider.autoDispose<String>(
   (ref) => Stream.periodic(Duration(seconds: 1), (count) => count.toString()),
 );
 
@@ -11,28 +11,18 @@ class ProviderScreen extends ConsumerWidget {
   Widget build(BuildContext context, watch) {
     return Scaffold(
       body: Center(
-        child: buildStreamBuilder(watch),
+        child: buildStreamWhen(watch),
       ),
     );
   }
 
-  Widget buildStreamBuilder(watch) {
-    final stream = watch(streamProvider.stream);
-    return StreamBuilder<String>(
-      stream: stream,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return CircularProgressIndicator();
-          default:
-            if (snapshot.hasError) {
-              return TextWidget('Error: ${snapshot.error}');
-            } else {
-              final counter = snapshot.data;
-              return TextWidget(counter);
-            }
-        }
-      },
+  Widget buildStreamWhen(watch) {
+    final stream = watch(streamProvider);
+
+    return stream.when(
+      data: (value) => TextWidget(value),
+      loading: () => CircularProgressIndicator(),
+      error: (e, stack) => TextWidget('Error: $e'),
     );
   }
 }
