d4e53ce30aaa12685119b031a1b0fda31603edc2
SinaSystem <sinasystem47@gmail.com>
2021-04-16 23:58:11 +0430

    Some little changes AutoDisposeInModifiers

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/text_widget.dart';
 
-
-
 final futureProvider =
 FutureProvider.autoDispose<String>((ref) => fetchValue());
+
 
 Future<String> fetchValue() async {
   await Future.delayed(Duration(seconds: 3));
   return 'State will be disposed!';
 }
+
 
 class AutoDisposeModifierPage extends ConsumerWidget {
   @override
@@ -18,9 +18,19 @@ class AutoDisposeModifierPage extends ConsumerWidget {
     final future = watch(futureProvider);
 
     return Scaffold(
-      body: future.when(
-        data: (value) => TextWidget(value.toString()),
-        loading: () => CircularProgressIndicator(),
-        error: (e, stack) => TextWidget('Error: $e'),
-      ),);}
+      appBar: AppBar(
+        title: Text('AutoDispose Modifier'),
+      ),
+      body: Center(
+        child: future.when(
+          data: (value) => Padding(
+            padding: EdgeInsets.all(48),
+            child: TextWidget(value.toString()),
+          ),
+          loading: () => CircularProgressIndicator(),
+          error: (e, stack) => TextWidget('Error: $e'),
+        ),
+      ),
+    );
+  }
 }
