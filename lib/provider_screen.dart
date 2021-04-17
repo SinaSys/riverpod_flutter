e9df288456620f73951a15b788e739ec4d5f11dc
SinaSystem <sinasystem47@gmail.com>
2021-04-17 14:06:02 +0430

    Commit CombiningProvidersExampleTwo

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -1,37 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_flutter/text_widget.dart';
 
 
-final cityProvider = Provider<String>((ref) => 'Munich2');
+final _counterProvider = StateProvider<int>((ref) => 2);
 
-final futureProvider = FutureProvider<int>((ref) async {
-  final city = ref.watch(cityProvider);
-  return fetchWeather(city);
+final _isEvenProvider = Provider<bool>((ref) {
+  final counter = ref.watch(_counterProvider);
+  return (counter.state % 2 == 0);
 });
 
-Future<int> fetchWeather(String city) async {
-  await Future.delayed(Duration(seconds: 2));
-  return city == 'Munich' ? 20 : 15;
+class CounterIsEven extends ConsumerWidget {
+  const CounterIsEven({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isEven = watch(_isEvenProvider);
+    return Material(child: Center(child: Text(isEven ? 'Is even' : 'Not even')));
+  }
 }
 
+class CounterStateProviderPage extends ConsumerWidget {
+  const CounterStateProviderPage({Key key}) : super(key: key);
 
-
-class ProviderScreen extends ConsumerWidget {
   @override
-  Widget build(BuildContext context, watch) {
-    final future = watch(futureProvider);
-
+  Widget build(BuildContext context, ScopedReader watch) {
+    final counter = watch(_counterProvider).state;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Combining Providers'),
-      ),
       body: Center(
-        child: future.when(
-          data: (value) => TextWidget(value.toString()),
-          loading: () => CircularProgressIndicator(),
-          error: (e, stack) => TextWidget('Error: $e'),
+        child: Column(
+          children: [
+            Text('Count: $counter'),
+            const CounterIsEven(),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read(_counterProvider).state++,
+        child: Icon(Icons.add),
       ),
     );
   }
